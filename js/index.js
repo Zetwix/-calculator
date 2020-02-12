ed2ffd517edf43b517cc589ca04c9fa31518d402
Zetwix <olehdobrii@gmail.com>
2020-02-12 14:28:12 +0000

    calc

--- js/index.js
+++ js/index.js
@@ -1,47 +1,51 @@
-const display = document.querySelector('.display');
+const display = document.querySelector(".display");
 
-document.querySelectorAll('.digits button')
-    .forEach ( button => button.addEventListener('click', digitPresed));
-    function rest(){
-        if ( display.value === 0)
-        display.value = button.addEventListener('click', digitPresed);
-    }
+document
+  .querySelectorAll(".digits button")
+  .forEach(button => button.addEventListener("click", digitPresed));
+function rest() {
+  if (display.value === 0)
+    display.value = button.addEventListener("click", digitPresed);
+}
 
 function digitPresed(ev) {
-    display.value += ev.target.innerText;
+  display.value += ev.target.innerText;
 }
 
-document.querySelectorAll('.opers button')
-    .forEach ( button => button.addEventListener('click', opersPresed));
-    
+document
+  .querySelectorAll(".opers button")
+  .forEach(button => button.addEventListener("click", opersPresed));
 
 function opersPresed(ev) {
+  if (
+    display.value.slice(-1) === "+" ||
+    display.value.slice(-1) === "*" ||
+    display.value.slice(-1) === "/" ||
+    display.value.slice(-1) === "-" 
+  ) {
+    display.value =
+      display.value.substring(0, display.value.length - 1) +
+      ev.target.innerText;
+  } else {
     display.value += ev.target.innerText;
-    if(value.split('')[value.length]==='+'||'-'||'/'||'*')
-    display.value = false;
+  }
 }
 
-document.querySelector('.eq').addEventListener('click', eqPresed);
+document.querySelector(".eq").addEventListener("click", eqPresed);
 
 function eqPresed() {
-    display.value = eval(display.value);
+  display.value = eval(display.value);
 }
 
-
-
-document.querySelector('.pow').addEventListener('click', doro);
+document.querySelector(".pow").addEventListener("click", doro);
 function doro() {
-    display.value *= display.value;
-    eval(display.value);
+  display.value *= display.value;
+  eval(display.value);
 }
 
-
-document.querySelector('.delete').addEventListener('click', deleted);
-    function deleted() {
-        display.value = "";
-    }
-
-
-
+document.querySelector(".delete").addEventListener("click", deleted);
+function deleted() {
+  display.value = "";
+}
 
     
